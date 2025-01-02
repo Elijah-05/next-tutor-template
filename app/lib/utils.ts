a5c832d8094c3c56728bb3aa9e4e9bc7eb807c35
ellayab804@gmail.com <ellayab804@gmail.com>
2025-01-03 00:29:00 +0300

    feat: search and pagination

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -39,7 +39,8 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   // If the total number of pages is 7 or less,
   // display all pages without any ellipsis.
   if (totalPages <= 7) {
-    return Array.from({ length: totalPages }, (_, i) => i + 1);
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    return pages
   }
 
   // If the current page is among the first 3 pages,
